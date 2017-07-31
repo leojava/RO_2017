787ca8b878d5ddb7100f3ff5fd6117e9234d1901
Leonardo Brutesco <brutesco.leo@gmail.com>
2017-07-31 21:47:18 +0200

    makeImg (bleeding)

--- makeImg/makeImg.js
+++ makeImg/makeImg.js
@@ -1,40 +1,47 @@
 function doCose(){
-var t=document.getElementById("t");
+	var t=document.getElementById("t");
 	var v= t.value;
-	var trasp=true;
+	var trasp=false;
+	trasp=document.getElementById("trasparenze").checked;
+	trasp=false;
 	
 	rects=v.split("\n");
-	var D=Number(rects[0]);
+	var D=0;
+	var bleeding=0;
+	{
+		var params=rects[0].split(",");
+		D=Number(params[0]);
+		bleeding=Number(params[1]);
+	}
 	var c=document.getElementById("c");
 	c.width=D;
 	c.height=D;
 
-
 	var context = c.getContext("2d");
-	
-	colors= ["azure","white", "red", "yellow", "blue", "green", "pink", "purple", "grey", "orange" ];
+	//colors= ["azure","white", "red", "yellow", "blue", "green", "pink", "purple", "grey", "orange" ];
+	//colors= ["#007fff","#ffffff", "#ff0000", "#ffff00", "#0000ff", "#00ff00", "#ff69b4", "#551a8b", "#d3d3d3", "#ffa500" ];
+	colors= ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff", "#b00b55", "#551a8b", "#777777", "#ffa500" ];
 	var i=1;
-	if(trasp){
-		//context.fillStyle = "white";
+	if(!trasp){
 		context.fillStyle = "black";
 		context.fillRect(0, 0, D, D);
 	}
 	for(;i<rects.length;i++){
-		rect=rects[i].split(",");
-		context.fillStyle = colors[i-1];
-		context.fillRect(rect[0], rect[1], rect[2], rect[3]);
+		var rect=rects[i].split(",");
+		var a={x:Number(rect[0]),y:Number(rect[1]),w:Number(rect[2]),h:Number(rect[3]),r:Number(rect[4])};
+		if(bleeding){
+			context.fillStyle = "#ffffff";
+			context.fillRect(a.x, a.y, a.w, a.h);
+			/*rect[0]+=1;
+			rect[1]+=1;
+			rect[2]-=1;
+			rect[3]-=1;*/
+		}
+		context.fillStyle = colors[(i-1)%colors.length];
+			context.fillRect(a.x+bleeding, a.y+bleeding, a.w-2*bleeding, a.h-2*bleeding);
 	}
-	
-/*	context.fillStyle = "green";
-	context.fillRect(1, 1, 100, 100);*/
-
 /*	var i=document.getElementById("i");
 	i.src=c.toDataURL("image/png");*/
 }
-window.setTimeout(doCose, 500);
 
-/*
-512
-1,1,100,100
-400,50,60,60
-*/
+window.setTimeout(doCose, 500);
